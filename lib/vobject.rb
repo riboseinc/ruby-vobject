d47c8e9f28d990dc85c7beccae3dcf0e3328b45f
Nick Nicholas <opoudjis@optusnet.com.au>
2017-07-03 20:46:59 +1000

    init

--- lib/vobject.rb
+++ lib/vobject.rb
@@ -1,187 +1,99 @@
-require "vobject/version"
+require "rsec"
+include Rsec::Helpers
 
 module Vobject
 
-  module Rules
+ class << self
 
-    module ABNF
-      IANAToken    = '[a-zA-Z\d\-]+?'
-      Cr           = "\u000d"
-      Lf           = "\u000a"
-      Crlf         = "(#{Cr}|#{Lf})"
-      Utf8_tail    = '[\u0080-\u00bf]'
-      Utf8_2       = '([\u00c2-\u00df]|' + "#{Utf8_tail})"
-      Utf8_3       = '([\u00e0\u00a0-\u00bf\u00e1-\u00ec\u00ed\u0080-\u009f\u00ee-\u00ef]|' + "#{Utf8_tail})"
-      Utf8_4       = '([\u00f0\u0090-\u00bf\u00f1-\u00f3\u00f4\u0080-\u008f]|' + "#{Utf8_tail})"
-      Wsp          = '[ \t]'
-      VChar        = '[\u0021-\u007e]'
-      NonASCII     = "(#{Utf8_2}|#{Utf8_3}|#{Utf8_4})"
-      QSafeChar    = "(#{Wsp}|" + '[!\u0023-\u007e]' + "|#{NonASCII})"
-      SafeChar     = "(#{Wsp}|" + '[!\u0023-\u0039\u003c-\u007e]' + "|#{NonASCII})"
-      ValueChar    = "(#{Wsp}|#{VChar}|#{NonASCII})"
-      DQuote       = '"'
-      PText        = "#{SafeChar}*?"
-      QuotedString = "#{DQuote}(#{QSafeChar}*?)#{DQuote}"
-      XName        = "[xX]-#{IANAToken}"
-      Group        = IANAToken
-      Name         = "(#{XName}|#{IANAToken})"
-      ParamName    = "(#{XName}|#{IANAToken})"
-      ParamValue   = "(#{PText}|#{QuotedString})"
-      PValueList   = "(?<head>#{ParamValue})(?<tail>(,#{ParamValue})*)"
-      Pid          = '\d+(\.\d+)*'
-      PidList      = "(?<head>#{Pid})(?<tail>(,#{Pid})*)"
-      Param        = "(?<pname>#{ParamName})=(?<pvalue>#{PValueList})"
-      ParamX        = /\A#{Param}\Z/
-      Params       = "(;(?<phead>#{Param}))(?<ptail>(;#{Param})*)"
-      ParamsX        = /\A#{Params}\Z/
-      Value        = "#{ValueChar}*?"
-      LineGroup    = "((?<group>#{Group})" + '\.' + ")?"
-      Contentline  = "#{LineGroup}(?<key>#{Name})(?<params>(#{Params})?):(?<value>#{Value})#{Crlf}"
-      ContentlineX        = /\A#{Contentline}\Z/
-      BeginLine    = "BEGIN:#{IANAToken}#{Crlf}"
-      VersionLine  = "VERSION:#{Value}#{Crlf}"
-      EndLine      = "END:#{IANAToken}#{Crlf}"
-      Vobject      = "#{BeginLine}#{VersionLine}(#{Contentline})+#{EndLine}"
-      VobjectX      = /\A#{Vobject}\Z/
-    end
+  def vobjectGrammar
+    ianaToken = /[a-zA-Z\d\-]+/.r {|s| s }
+    utf8_tail = /[\u0080-\u00bf]/.r
+    utf8_2 = /[\u00c2-\u00df]/.r  | utf8_tail
+    utf8_3 = /[\u00e0\u00a0-\u00bf\u00e1-\u00ec\u00ed\u0080-\u009f\u00ee-\u00ef]/.r  | utf8_tail
+    utf8_4 = /[\u00f0\u0090-\u00bf\u00f1-\u00f3\u00f4\u0080-\u008f]/.r | utf8_tail
+    nonASCII = utf8_2 | utf8_3 | utf8_4
+    wsp = /[ \t]/.r
+    qSafeChar = wsp | /[!\u0023-\u007e]/ | nonASCII
+    safeChar = wsp | /[!\u0023-\u0039\u003c-\u007e]/  | nonASCII
+    vChar = /[\u0021-\u007e]/.r
+    valueChar = wsp | vChar | nonASCII
+    dQuote = /"/
+    beginLine = seq('BEGIN:'.r , ianaToken , /[\r\n]/).fail('begin')  {|_, token, _|
+			{ :BEGIN => token.to_sym }
+	}
+    endLine = seq('END:' , ianaToken , /[\r\n]/) { |_, token, _|
+			{ :END => token.to_sym }
+        }
+    group = ianaToken
+    xname = seq( '[xX]-', ianaToken)
+    linegroup = seq( group,  '.' ) {|group, _| group }
+    beginend = 'BEGIN'.r | 'END'.r
+    name  = ( xname | seq(''.r ^ beginend , ianaToken )[1]).fail('name')
+    paramname = xname | ianaToken
+    pText  = safeChar.star.map(&:join)
+    quotedString = seq(dQuote, qSafeChar.star, dQuote) {|_, qSafe, _| qSafe.join('') }
+    paramvalue = pText.map | quotedString
+    
+    pvalueList = paramvalue.map {|e| e.sub(Regexp.new("^\"(.+)\"$"), '\1').gsub(/\\n/, "\n")  }  |
+	    seq(paramvalue, ','.r, lazy{pvalueList} ) {|e, _, list|
+			e.sub(Regexp.new("^\"(.+)\"$"), '\1').gsub(/\\n/, "\n") + "," + list
+	}
+    param = seq(paramname, '=', pvalueList) {|name, _, list|
+			{name.to_sym => list}
+	}
+    params = seq( ';'.r, param) {|_, p|
+			[ p ]
+	} 
+    params = params | seq(';'.r, param, params ) {|_, p, ps|
+			[ p ] + ps
+	}
+    value = valueChar.star.map(&:join)
+    contentline = seq( linegroup._?, name, params._?, ':', value, /[\r\n]/).fail('contentline') {|linegroup, name, params, _, value, _|
+			key =  name.to_sym
+			hash = { key => {} }
+			hash[key][:value] = value
+			hash[key][:group] = linegroup  unless linegroup.empty?
+			hash[key][:params] = params unless params.empty?
+			hash
+	}
+    rest = endLine.fail('rest1').map {|e| []} |
+	    seq(contentline, lazy{rest}).fail('rest2') {|(contentline, rest)|
+		[ contentline ]  + rest
+    } | seq(beginLine, lazy{rest}, lazy{rest}).fail('rest3') {|(beginline, rest0, rest1)|
+		[ { beginline[:BEGIN] => rest0 } ] + rest1
+	}
+    versionLine = seq( 'VERSION:'.r , value , /[\r\n]/) {|_, version, _|
+			hash = { :VERSION => {} }
+			hash[:VERSION][:value] = version
+			hash
+	}
+    #pid = /\d+(\.\d+)*/
+    #pidList = seq(pid, seq(',', Pid).star)
+    vobject = seq(beginLine, versionLine, rest).fail('vobject') { |(beginline, versionline, rest)|
+			comp = beginline[:BEGIN].to_sym
+	            	hash = { comp => [versionline] + rest }
+	            hash
+	}
 
+    vobject.eof 
+  end 
+
+  def parse(vobject)
+	ret = vobjectGrammar.parse(unfold(vobject))
+	Rsec::Fail.reset
+	return ret
   end
 
-  class << self
+private
 
-    def parse(vobject)
-      vobject = unfold(vobject)
-      lines   = []
-      rule    = /\A(?<line>#{Rules::ABNF::Contentline})(?<remainder>(#{Rules::ABNF::Contentline})*)\Z/
-
-      parse_for_rule(Rules::ABNF::VobjectX, vobject) do |parsed|
-
-        remainder = vobject
-
-        while !remainder.empty?
-          parse_for_rule(rule, remainder) do |remainder_parsed|
-            lines << remainder_parsed[:line]
-            remainder = remainder_parsed[:remainder]
-          end
-        end
-      end
-
-      parse_lines lines
-    end
-
-    private
-
-    def unfold(str)
-      str.gsub(/#{Rules::ABNF::Crlf}#{Rules::ABNF::Wsp}/, '')
-    end
-
-    def parse_lines lines
-      lines.each_with_index.reduce([]) do |hash_stack, (line, i)|
-        prop = parse_line(line)
-
-        if prop.has_key?(:BEGIN)
-          comp = prop[:BEGIN][:value].to_sym
-          hash = { comp => [] }
-          next hash_stack << hash
-        end
-
-        if prop.has_key?(:END)
-          hash = hash_stack.pop
-          comp = hash.keys.first
-
-          raise_invalid_parsing if comp != prop[:END][:value].to_sym
-
-          prev_hash = hash_stack.last
-
-          raise_invalid_parsing if !prev_hash && i != lines.length - 1
-
-          return hash unless prev_hash
-
-          prev_hash[prev_hash.keys.first] << hash
-
-          next hash_stack
-        end
-
-        prev_hash = hash_stack.last
-
-        prev_hash[prev_hash.keys.first] << prop
-        hash_stack
-      end
-    end
-
-    def parse_line(line)
-      parse_for_rule(Rules::ABNF::ContentlineX, unfold(line)) do |parsed|
-        key = parsed[:key].to_sym
-
-        group = parsed[:group]
-        params = parse_params(parsed[:params])
-        value = parsed[:value]
-
-        hash = { key => {} }
-        hash[key][:group] = group if group
-        hash[key][:params] = params if !params.empty?
-        hash[key][:value] = value
-
-        hash
-      end
-    end
-
-    def parse_params(params_str)
-      params = {}
-
-      while !params_str.empty?
-        parse_for_rule(Rules::ABNF::ParamsX, params_str) do |parsed|
-
-          parse_for_rule(Rules::ABNF::ParamX, parsed[:phead]) do |param_parsed|
-            pname  = param_parsed[:pname].to_sym
-            pvalue = param_parsed[:pvalue].sub(
-              Regexp.new("^#{Rules::ABNF::QuotedString}$"),
-              '\1'
-            )
-
-            pvalue.gsub!(/\\n/, "\n")
-
-            params[pname] = if params[pname]
-              "#{params[pname]},#{pvalue}"
-            else
-              pvalue
-            end
-          end
-
-          params_str = parsed[:ptail]
-        end
-      end
-
-      params
-    end
-
-    #Method: parse_for_rule
-    #Parameter: String containing the regular expression, String to be parsed
-    #and optional block to indicate whether to yield the resulting hash
-    #Return: a hash with keys indicating their regex names
-    def parse_for_rule(rule, str, &block)
-      #matched = /\A#{rule}\Z/.match(str)
-      matched = rule.match(str)
-
-      raise_invalid_parsing unless matched
-
-      parsed = matched.names.reduce({}) do |parsed_hash, name|
-        #can we reduce memory consumption by only creating Keys whose value is not nil?
-        parsed_hash[name.to_sym] = matched[name.to_sym] if matched[name.to_sym]
-        #parsed_hash[name.to_sym] = matched[name.to_sym]
-        parsed_hash
-      end
-
-      return yield(parsed) if block
-
-      parsed
-    end
-
-    def raise_invalid_parsing
-      raise "VObject parse failed"
-    end
-
+  def unfold(str)
+	         str.gsub(/[\n\r]+[ \t]+/, '')
   end
 
+
+   def raise_invalid_parsing
+	          raise "VObject parse failed"
+   end
+
+  end
 end
-
