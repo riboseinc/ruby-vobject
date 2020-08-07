b47336115f87718ab9cb412ac21a9e3dc9fdb99a
Sebastian Skałacki <skalee@gmail.com>
2020-08-07 23:29:09 +0200

    Add Pry as development dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require "rspec/json_expectations"
 require "json"
+require "pry"
+require "pry-doc"
 
 require "vobject"
 require "vcalendar"
